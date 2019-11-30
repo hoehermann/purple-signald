1d72edc0b2454603b729db25342bccc93448704c
Hermann Kraus <hermr2d2+git@gmail.com>
2019-11-30 20:51:24 +0100

    Handle attachments (e.g. images).

--- libsignald.c
+++ libsignald.c
@@ -25,6 +25,7 @@
 #include <errno.h>
 #include <sys/socket.h> // for recv
 #include <sys/un.h> // for sockaddr_un
+#include <sys/stat.h> // for chmod
 
 #ifdef ENABLE_NLS
 // TODO: implement localisation
@@ -95,11 +96,32 @@ signald_assume_all_buddies_online(SignaldAccount *sa)
 void
 signald_process_message(SignaldAccount *sa,
         const gchar *username, const gchar *content, time_t timestamp,
-        const gchar *groupid_str, const gchar *groupname)
+        const gchar *groupid_str, const gchar *groupname, GString *attachments)
 {
     PurpleMessageFlags flags = PURPLE_MESSAGE_RECV;
+    if (attachments->len) {
+        flags |= PURPLE_MESSAGE_IMAGES;
+    }
     const gchar * sender = groupid_str && *groupid_str ? groupid_str : username;
-    purple_serv_got_im(sa->pc, sender, content, flags, timestamp);
+    g_string_append(attachments, content);
+    purple_serv_got_im(sa->pc, sender, attachments->str, flags, timestamp);
+}
+
+void
+signald_parse_attachment(SignaldAccount *sa, JsonObject *obj, GString *message)
+{
+    const char *type = json_object_get_string_member(obj, "contentType");
+    const char *fn = json_object_get_string_member(obj, "storedFilename");
+    if (purple_strequal(type, "image/jpeg") || purple_strequal(type, "image/png")) {
+        PurpleStoredImage *img = purple_imgstore_new_from_file(fn);
+        size_t size = purple_imgstore_get_size(img);
+        int img_id = purple_imgstore_add_with_id(g_memdup(purple_imgstore_get_data(img), size), size, NULL);
+        g_string_append_printf(message, "<IMG ID=\"%d\"/><br/>", img_id);
+    } else {
+        //TODO: Receive file using libpurple API
+        g_string_append_printf(message, "<a href=\"file://%s\">Attachment (type: %s)</a><br/>", fn, type);
+    }
+    purple_debug_info(SIGNALD_PLUGIN_ID, "Attachment: %s", message->str);
 }
 
 void
@@ -118,6 +140,12 @@ signald_parse_message(SignaldAccount *sa, JsonObject *obj)
         time_t timestamp = json_object_get_int_member(obj, "timestamp") / 1000;
         obj = json_object_get_object_member(obj, "dataMessage");
         const gchar *message = json_object_get_string_member(obj, "message");
+        JsonArray *attachments = json_object_get_array_member(obj, "attachments");
+        guint len = json_array_get_length(attachments);
+        GString *attachments_message = g_string_sized_new(len * 100); //Preallocate buffer
+        for (guint i=0; i < len; i++) {
+            signald_parse_attachment(sa, json_array_get_object_element(attachments, i), attachments_message);
+        }
         obj = json_object_get_object_member(obj, "groupInfo");
         const gchar *groupid_str = NULL;
         const gchar *groupname = NULL;
@@ -125,8 +153,9 @@ signald_parse_message(SignaldAccount *sa, JsonObject *obj)
             groupid_str = json_object_get_string_member(obj, "groupId");
             groupname = json_object_get_string_member(obj, "name");
         }
-        purple_debug_info(SIGNALD_PLUGIN_ID, "New message from %s: %s\n", username, message);
-        signald_process_message(sa, username, message, timestamp, groupid_str, groupname);
+        purple_debug_info(SIGNALD_PLUGIN_ID, "New message from %s (%d attachments): %s %p\n", username, len, message, attachments);
+        signald_process_message(sa, username, message, timestamp, groupid_str, groupname, attachments_message);
+        g_string_free(attachments_message, TRUE);
     }
 }
 
@@ -270,6 +299,7 @@ signald_login(PurpleAccount *account)
     pc_flags |= PURPLE_CONNECTION_NO_FONTSIZE;
     pc_flags |= PURPLE_CONNECTION_NO_BGCOLOR;
     pc_flags |= PURPLE_CONNECTION_ALLOW_CUSTOM_SMILEY;
+    pc_flags |= PURPLE_CONNECTION_HTML;
     purple_connection_set_flags(pc, pc_flags);
 
     SignaldAccount *sa = g_new0(SignaldAccount, 1);
@@ -358,15 +388,82 @@ signald_send_im(PurpleConnection *pc,
                 const gchar *who, const gchar *message, PurpleMessageFlags flags)
 {
 #endif
-    char *plain = purple_unescape_html(message);
     purple_debug_info(SIGNALD_PLUGIN_ID, "signald_send_im: flags: %x msg:%s\n", flags, message);
     SignaldAccount *sa = purple_connection_get_protocol_data(pc);
-    // build json
+
     JsonObject *data = json_object_new();
     json_object_set_string_member(data, "type", "send");
     json_object_set_string_member(data, "username", purple_account_get_username(sa->account));
     json_object_set_string_member(data, who[0]=='+' ? "recipientNumber" : "recipientGroupId", who);
+
+    // Search for embedded images and attach them to the message. Remove the <img> tags.
+    JsonArray *attachments = json_array_new();
+    GString *msg = g_string_new("");
+    GData *attribs;
+    const char *start, *end, *last;
+    last = message;
+
+    /* for each valid IMG tag... */
+    while (last && *last && purple_markup_find_tag("img", last, &start, &end, &attribs))
+    {
+        PurpleStoredImage *image = NULL;
+        const char *id;
+
+        if (start - last) {
+            g_string_append_len(msg, last, start - last);
+        }
+
+        id = g_datalist_get_data(&attribs, "id");
+
+        /* ... if it refers to a valid purple image ... */
+        if (id && (image = purple_imgstore_find_by_id(atoi(id)))) {
+            unsigned long size = purple_imgstore_get_size(image);
+            gconstpointer imgdata = purple_imgstore_get_data(image);
+            gchar *tmp_fn = NULL;
+            GError *error = NULL;
+            //TODO: This is not very secure. But attachment handling should be reworked in signald to allow sending them in the same stream as the message
+            //Signal requires the filename to end with a known image extension. However it does not care if the extension matches the image format.
+            //contentType is ignored completely.
+
+            gint file = g_file_open_tmp("XXXXXX.png", &tmp_fn, &error);
+            if (file == -1) {
+                purple_debug_error(SIGNALD_PLUGIN_ID, "Error: %s\n", error->message);
+            } else {
+                close(file);
+                error = NULL;
+                if (!g_file_set_contents(tmp_fn, imgdata, size, &error)) {
+                    purple_debug_error(SIGNALD_PLUGIN_ID, "Error: %s\n", error->message);
+                } else {
+                    chmod(tmp_fn, 0666);
+                    JsonObject *attachment = json_object_new();
+                    json_object_set_string_member(attachment, "filename", tmp_fn);
+//                    json_object_set_string_member(attachment, "caption", "Caption");
+//                    json_object_set_string_member(attachment, "contentType", "image/png");
+//                    json_object_set_int_member(attachment, "width", 150);
+//                    json_object_set_int_member(attachment, "height", 150);
+                    json_array_add_object_element(attachments, attachment);
+                }
+                g_free(tmp_fn);
+                //TODO: Check for memory leaks
+                //TODO: Delete file when response from signald is received
+            }
+        }
+        /* If the tag is invalid, skip it, thus no else here */
+
+        g_datalist_clear(&attribs);
+
+        /* continue from the end of the tag */
+        last = end + 1;
+    }
+
+    /* append any remaining message data */
+    if (last && *last)
+        g_string_append(msg, last);
+
+    json_object_set_array_member(data, "attachments", attachments);
+    char *plain = purple_unescape_html(msg->str);
     json_object_set_string_member(data, "messageBody", plain);
+    g_string_free(msg, TRUE);
     g_free(plain);
     if (!signald_send_json(sa, data)) {
         return -errno;
@@ -495,7 +592,7 @@ plugin_init(PurplePlugin *plugin)
 
 	info->extra_info = prpl_info;
 
-    prpl_info->options = OPT_PROTO_NO_PASSWORD;
+    prpl_info->options = OPT_PROTO_NO_PASSWORD | OPT_PROTO_IM_IMAGE;
     prpl_info->protocol_options = signald_add_account_options(prpl_info->protocol_options);
 
     /*

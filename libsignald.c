24298ce53e87953648620833db003a58cef7835a
Torsten <mttl@mailbox.org>
2020-02-22 18:32:51 +0100

    moved data dir of signald from plugins directory into into ~/.purple/signald

--- libsignald.c
+++ libsignald.c
@@ -61,14 +61,13 @@
 #define SIGNALD_DEFAULT_SOCKET_LOCAL "/tmp/signald.sock"
 #define SIGNALD_START "signald -s " SIGNALD_DEFAULT_SOCKET_LOCAL " -d " SIGNALD_DATA_PATH " &"
 
-#define SIGNALD_DATA_PATH "%s/plugins/signald"
+#define SIGNALD_DATA_PATH "%s/signald"
 #define SIGNALD_DATA_FILE SIGNALD_DATA_PATH "/data/%s"
 #define SIGNALD_PID_FILE SIGNALD_DATA_PATH "/pid"
 
 #define SIGNALD_TMP_QRFILE "/tmp/signald_link_purple_qrcode.png"
 #define SIGNALD_PID_FILE_QR SIGNALD_DATA_PATH "/pidqr"
 #define SIGNALD_QRCREATE_CMD "qrencode -s 6 -o " SIGNALD_TMP_QRFILE " '%s'"
-#define SIGNALD_QRCREATE_MAXLEN 512
 #define SIGNALD_QR_MSG "echo Link by scanning QR with Signal App"
 #define SIGNALD_LINK_TYPE "linking_"
 

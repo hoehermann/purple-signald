5a9c69525a1e039b20cdf2f57bd003c8a2c3b848
Torsten <mttl@mailbox.org>
2021-10-10 15:51:14 +0200

    move list group request to v1, but still not working

--- groups.c
+++ groups.c
@@ -553,8 +553,8 @@ signald_request_group_list(SignaldAccount *sa)
     JsonObject *data = json_object_new();
 
     json_object_set_string_member(data, "type", "list_groups");
-    json_object_set_string_member(data, "username", purple_account_get_username(sa->account));
-//    json_object_set_string_member(data, "version", "v0");
+    json_object_set_string_member(data, "account", purple_account_get_username(sa->account));
+    json_object_set_string_member(data, "version", "v1");
 
     if (!signald_send_json(sa, data)) {
         purple_connection_error(sa->pc, PURPLE_CONNECTION_ERROR_NETWORK_ERROR, _("Could not request contacts."));

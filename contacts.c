7cb7fac11c0f8d6415581573377c331e7f9acce7
Hermann Höhne <hoehermann@gmx.de>
2022-04-05 23:49:03 +0200

    Ability to query profile information.
    Implements #28.

--- contacts.c
+++ contacts.c
@@ -106,6 +106,63 @@ signald_parse_contact_list(SignaldAccount *sa, JsonArray *profiles)
     //TODO: mark buddies not in contact list but in buddy list as "deleted"
 }
 
+/*
+ * Purple function: Request information about a buddy.
+ * 
+ * See @signald_process_profile on how the answer is parsed.
+ */
+void
+signald_get_info(PurpleConnection *pc, const char *who) {
+    SignaldAccount *sa = purple_connection_get_protocol_data(pc);
+    g_return_if_fail(sa->uuid);
+    JsonObject *message = json_object_new();
+    json_object_set_string_member(message, "type", "get_profile");
+    json_object_set_string_member(message, "account", sa->uuid);
+    JsonObject *address = json_object_new();
+    json_object_set_string_member(address, "uuid", who);
+    json_object_set_object_member(message, "address", address);
+    if (!signald_send_json(sa, message)) {
+        purple_connection_error(sa->pc, PURPLE_CONNECTION_ERROR_NETWORK_ERROR, _("Could not send request to get profile.."));
+    }
+    json_object_unref(message);
+}
+
+/*
+ * Helper function for @signald_process_profile.
+ */
+static void
+signald_process_profile_info_member(JsonObject *object, const gchar *member_name, JsonNode *member_node, gpointer user_data) {
+    if (JSON_NODE_HOLDS_OBJECT(member_node)) {
+        JsonObject *obj = json_node_get_object(member_node);
+        json_object_foreach_member(obj, signald_process_profile_info_member, user_data);
+    }
+    if (JSON_NODE_HOLDS_VALUE(member_node)) {
+        PurpleNotifyUserInfo *user_info = user_data;
+        GValue value = G_VALUE_INIT;
+        json_node_get_value(member_node, &value);
+        GValue string = G_VALUE_INIT;
+        g_value_init(&string, G_TYPE_STRING);
+        g_value_transform(&value, &string);
+        purple_notify_user_info_add_pair_plaintext(user_info, member_name,  g_value_get_string(&string));
+    }
+}
+
+/*
+ * Recursively flattens profile information into a sequence of key value pairs for information display.
+ */
+void
+signald_process_profile(SignaldAccount *sa, JsonObject *obj) {
+    JsonObject *address = json_object_get_object_member(obj, "address");
+    g_return_if_fail(address);
+    const char *uuid = json_object_get_string_member(address, "uuid");
+    g_return_if_fail(uuid && uuid[0]);
+    
+    PurpleNotifyUserInfo *user_info = purple_notify_user_info_new();
+    json_object_foreach_member(obj, signald_process_profile_info_member, (gpointer) user_info);
+    purple_notify_userinfo(sa->pc, uuid, user_info, NULL, NULL);
+    purple_notify_user_info_destroy(user_info);
+}
+
 void
 signald_add_buddy(PurpleConnection *pc, PurpleBuddy *buddy, PurpleGroup *group
 #if PURPLE_VERSION_CHECK(3, 0, 0)
